916096a68ee92d5f10d2fcb372b57cda76890abb
hugoresende27 <Hugoresende27@gmail.com>
2021-09-18 23:13:18 +0100

    sab_n

--- exercicios youtube/poupanca.cpp
+++ exercicios youtube/poupanca.cpp
@@ -1,5 +1,7 @@
 /*
-Suppose you want to deposit a certain amount of money into a savings account and then leave it alone to draw interest for the next 10 years. At the end of 10 years you would like to have $10000 in the account. How much would you need to deposit today toi make that happen? You can use the following formula which is known as the percent value formula, to find out:
+Suppose you want to deposit a certain amount of money into a savings account and then leave it alone to draw interest for the next 10 years. 
+At the end of 10 years you would like to have $10000 in the account. How much would you need to deposit today toi make that happen? 
+You can use the following formula which is known as the percent value formula, to find out:
 		F
 P =   -----
       (1 + r) (to the power of n)
@@ -9,7 +11,9 @@ F - Future value that you want in the account (in this case, $10,000)
 r - annual interest rate
 n - number of years you plan to let the money sit in the account
 
-Write a program that has a function named presentValue that performs this calculation. The function should accept the future value, annual interest rate, and number of years in the arguments. It should return the present value, which is the amount that you need to deposit today.
+Write a program that has a function named presentValue that performs this calculation. The function should accept the future value,
+ annual interest rate, and number of years in the arguments. It should return the present value, which is the amount that you need to deposit 
+ today.
 Demonstrate the function in a program that lets the user experiment with different values for the formula's terms.
 
 */
