a85db4c2acf6e336bd8bd5ae0b716f2c608caecd
hugoresende27 <Hugoresende27@gmail.com>
2021-10-08 15:46:31 +0100

    sex

--- Aula_12(operadores ternários)/exc3.cpp
+++ Aula_12(operadores ternários)/exc3.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main ()
 {
 	char nome[MAX];
-    char situacao[MAX];
+    char situacao[MAX]=" ";//se não for inicializada, ao concatenar com "Aprovado" ou "Reprovado" pode trazer lixo, dados de outro lado
     float nota;
     printf("Qual o nome?");
-    gets(nome);
+    scanf("%s",nome);
     printf("Qual a nota?");
     scanf("%f",&nota);
     nota>=10 ? (strcat(situacao,"Aprovado")) : (strcat(situacao,"Reprovado"));
-    printf("\n\tNome\tNota\t  Situação");
-    printf("\n\t %s \t %.1f \t %s\n",nome,nota,situacao);
+    printf("\n\tNome\tNota\tSituação");
+    printf("\n\t%s \t%.1f \t%s\n",nome,nota,situacao);
 	system("pause");
 	return 0;
 }
