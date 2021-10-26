15fd576627201343b9fa94d3aab6dc27e7b91790
hugoresende27 <Hugoresende27@gmail.com>
2021-10-26 12:13:01 +0100

    ter

--- Exercicios_PDF/exc9_3vetores_ele_comum.cpp
+++ Exercicios_PDF/exc9_3vetores_ele_comum.cpp
@@ -9,6 +9,48 @@ using namespace std;
 
 int main()								
 {
+    int contador=0;
+    float vetorA[5],vetorB[5],vetorS[5];
+    for (int i = 0;i<5;i++)
+    {
+        printf ("vetorA[%d]--> ",i);
+        scanf ("%f",&vetorA[i]);
+    }
+
+    for (int i = 0;i<5;i++)
+    {
+        printf ("vetorB[%d]--> ",i);
+        scanf ("%f",&vetorB[i]);
+    }
+
+    for (int i = 0;i<5;i++)
+    {
+        for (int j = 0; j<5;j++)
+        {
+            if (vetorA[i]==vetorB[j])
+                {
+                    vetorS[i]=vetorA[i];
+                    contador++;
+                }
+        }
+     
+    }
+    cout << "*******************************************\n";
+    for (int i = 0;i<5;i++)
+    {
+        printf ("\tvetorA[%d]--> %.1f\n", i , vetorA[i]); 
+    }
+    cout << "*******************************************\n";
+    for (int i = 0;i<5;i++)
+    {
+        printf ("\tvetorB[%d]--> %.1f\n", i , vetorB[i]); 
+    }
+    cout << "*******************************************\n";
+    for (int i = 0;i<contador;i++)
+    {
+        printf ("\tvetorS[%d]--> %.1f\n", i , vetorS[i]); 
+    }
+
     system("pause");				
 	return 0;	
 }
