7b83717905767175f5f7e81eaa3f95ba0d71a5d5
hugoresende27 <Hugoresende27@gmail.com>
2021-11-02 15:13:59 +0000

    ter

--- freeCodeCamp/Caesar_cipher.cpp
+++ freeCodeCamp/Caesar_cipher.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string encriptar (string txt, int s)
 {
-    string res=" ";
+    string res="";
     for (int i=0; i< txt.length(); i++)
     {
         if (isupper(txt[i]))
@@ -20,16 +20,46 @@ string encriptar (string txt, int s)
     return res;
 }
 
+string encriptar2 (string txt, int s)
+{
+    char ch;
+    for (int i=0; i< txt[i] != '\0'; ++i)
+    {
+        ch = txt[i];
+        if (ch>='a' && ch<='z')
+        {
+            ch -= s;
+            if (ch < 'a')
+            {
+                ch = ch + 'z'-'a'+1;
+            }
+        txt[i]=ch;
+        }
+        else if (ch>='A' && ch<='Z')
+        {
+            ch -= s;
+            if (ch < 'A')
+            {
+                ch = ch + 'Z'-'A'+1;
+            }
+        txt[i]=ch;
+        }
+    }
+    return txt;
+}
+
 
 int main()								
 {
-    char var[100];
+    char var[999];
     int num;
     cout << "Qual o texto? --> ";
+    //cin>> var;
     gets(var);
     cout<<"Valor da encriptação? --> ";
     cin >> num;
     cout << "O texto " << var << " vai ficar igual "<< encriptar(var,num);
+    cout << "O texto " << var << " vai ficar igual "<< encriptar2(var,num);
     system("pause");				
 	return 0;	
 }
