4b04261f0bc5ea11afea227cccde1a0300dd9e87
hugoresende27 <Hugoresende27@gmail.com>
2021-11-03 09:30:30 +0000

    qua

---
+++ Udemy C/aula_4_tipos_e_vars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char letra = 'A';
+    int numero = 123;
+    string palavra = "Hugo-@27";
+    double numero_real = 7.77;
+    return 0;
+}
