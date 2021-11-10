edd5762d7012121cc2f675baa9e134c447cc3f78
hugoresende27 <Hugoresende27@gmail.com>
2021-11-10 12:23:55 +0000

    d

--- Modulo 783/Aula 10/exc3.cpp
+++ Modulo 783/Aula 10/exc3.cpp
@@ -3,6 +3,8 @@ criar um programa q leia 10 num inteiros e imprima quantos estão entre 10 e 20
 */
 #include <iostream>
 #define MAX 10
+#define inf 10
+#define sup 20
 using namespace std;
 
 int main()
@@ -13,15 +15,23 @@ int main()
     {
         cout << "Num "<<i+1<<"--> ";
         cin >> num;
-        if (num>=10 && num<=20)
+        if (num>=inf && num<=sup)
         {
             dezVinte[tamVet]=num;
             tamVet++;
             soma+=num;
         }
+        else
+        {
+            cout << "Valor não contablizado, não está entre "<<inf<< " e "<<sup<<endl;
+        }
     }
-    cout << "Existem "<<tamVet<<" numeros entre 10 e 20\n";
-    cout << "A soma é "<<soma<<endl;
+    cout << "Existem "<<tamVet<<" numeros entre "<<inf<<" e "<<sup<<endl;
+    if (soma>=50 && soma <=100)
+    {
+        cout << "A soma é "<<soma<<endl;
+    }
+    
     for (int i=0; i<tamVet;i++)
     {
         cout <<dezVinte[i]<<" | ";
