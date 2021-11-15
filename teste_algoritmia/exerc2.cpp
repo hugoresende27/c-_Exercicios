d30f2e853911ab5a27f12ecc07c70a73ef56e7dd
hugoresende27 <Hugoresende27@gmail.com>
2021-11-15 12:15:16 +0000

    a

--- teste_algoritmia/exerc2.cpp
+++ teste_algoritmia/exerc2.cpp
@@ -1,70 +1,76 @@
 #include <iostream>	
-		
+#include <string.h>
+#include <iomanip>
 using namespace std;
 
 string nomes[3];
-int qtd[3];
-float precoMao[3],precoMadeira[3];
+
 int op;
 bool dados=false;
+float precoMadeira[3],precoMao[3];
 
-int menu();
+void menu();
 void inserir();
-void calcOrca();
+float orcamento();
+void p_BARATO();
+void p_pessoal();
 
 int main()								
 {
-if (dados==false);
+if (dados==false)
 {
     inserir();
+    menu();
 } 
 else
 {
-    do
-    {
-        switch (menu())
-        {
-        case 1:
-            inserir();
-            break;
-        
-        case 2:
-            calcOrca();
-            break;
-        
-        case 3:
-            inserir();
-            break;
-        
-        case 4:
-            inserir();
-            break;
-        case 5:
-            exit(1);
-            break;
-        
-        default:
-            break;
-        }
-    } while (op !=5 );
-
+    menu();
 }
 
     system("pause");				
 	return 0;	
 }
 
-int menu()
+void menu()
 {
-    cout << "********* MENU ************************\n";
-    cout << "*** 1.Inserir Dados *******************\n";
-    cout << "*** 2.Cálculo de orçamento padrão *****\n";
-    cout << "*** 3.Orçamento mais barato ***********\n";
-    cout << "*** 4.Cálculo de orçamento person *****\n";
-    cout << "*** 5.Sair *****\n";
-    cout << "******************* OPÇÂO --> ";
+
+    cout << "********* MENU ****************************\n";
+    cout << "*** 1.Inserir Dados ***********************\n";
+    cout << "*** 2.Cálculo de orçamento padrão  ********\n";
+    cout << "*** 3.Orçamento mais barato ***************\n";
+    cout << "*** 4.Cálculo de orçamento person *********\n";
+    cout << "*** 5.Sair ********************************\n";
+    cout << "************************** OPÇÂO --> ";
     cin >> op;
-    return op;
+
+    switch (op)
+    {
+        case 1:
+            inserir();
+            break;
+      
+        case 2:
+            printf("Vai custar %.2f euros \n\n", orcamento()) ;
+            break;
+        
+        case 3:
+            p_BARATO();
+            break;
+        
+        case 4:
+            p_pessoal();
+            break;
+        case 5:
+            exit(1);
+            break;
+        
+        default:
+            cout << "OPCAO INVÁLIDA... \n";
+            break;
+
+    } 
+   
+
 }
 
 void inserir()
@@ -74,19 +80,70 @@ void inserir()
     {
         cout << "Nome do fornecedor--> ";
         cin >> nomes[i];
-        // cout << "Preço da mão de obra--> ";
-        // cin >> precoMao[i];
-        // cout << "Quantidade madeira (kg)--> ";
-        // cin >> qtd[i];
-        // cout << "Preço da madeira--> ";
-        // cin >> precoMadeira[i];
+        cout << "Preço da mão de obra--> ";
+        cin >> precoMao[i];
+        cout << "Preço da madeira--> ";
+        cin >> precoMadeira[i];
     }
 }
 
-void calcOrca()
+float orcamento()
 {
+    int escolha;
+    float orca,qtd;
     for (int i=0; i<3; i++)
     {
-        cout << nomes[i]<<endl;
+        cout <<"Fornecedor " <<i+1<<"--> "<<nomes[i]<<endl;
+    }
+    cout << "Escolha Fornecedor (1/2/3)--> ";
+    cin >> escolha;
+    cout << "Quantidade madeira (kg)--> ";
+    cin >> qtd;
+    orca = (qtd * (1.15*precoMadeira[escolha-1]))*(1.1*precoMao[escolha-1]);
+    return orca;
+}
+
+void p_BARATO()
+{
+    float maisBarato[3],barato;
+    string fornecedorMaisBarato;
+    int qtd;
+    cout << "Quantidade madeira (kg)--> ";
+    cin >> qtd;
+
+    for (int i=0; i<3;i++)
+    {
+        maisBarato[i] = (qtd * (1.15*precoMadeira[i]))*(1.1*precoMao[i]);
+    }
+    barato = maisBarato[0];
+    for (int i=0; i<3;i++)
+    {
+        cout << maisBarato[i]<<endl;
+        if (maisBarato[i] < barato) 
+        {
+            barato = maisBarato[i];
+            fornecedorMaisBarato = nomes[i];
+            //strcpy(fornecedorMaisBarato,nomes[i]);
+        }
+    }
+    cout << "O mais barato é o "<< fornecedorMaisBarato<<" com o preço de " <<setprecision(2)<<barato<<" euros"<<endl;
+}
+
+void p_pessoal()
+{
+    float precosMad[3],precosMao[3],orcamentos[3];
+    int qtd;
+    cout << "Quantidade madeira (kg)--> ";
+    cin >> qtd;
+    for (int i=0; i<3;i++)
+    {
+        precosMad[i] = (qtd * (1.15*precoMadeira[i]));
+        precosMao[i] = (1.1*precoMao[i]);
+        orcamentos[i] = precosMad[i] * precosMao[i];
+    }
+    cout << "Fornecedor    Preços Madeira   Preços Mão de Obra\n";
+    for (int i=0; i<3;i++)
+    {
+        cout<< nomes[i]<< "  \t\t" << precosMad[i] <<"  \t\t"<< precosMao[i] <<"  \t"<<endl;
     }
 }
