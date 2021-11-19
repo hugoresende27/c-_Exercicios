11c75f40448588903d92da1050b903092726eb4c
hugoresende27 <Hugoresende27@gmail.com>
2021-11-19 16:55:10 +0000

    sex

--- Modulo 784 FuncoesEstruturas/atividade3.cpp
+++ Modulo 784 FuncoesEstruturas/atividade3.cpp
@@ -13,12 +13,41 @@ float grausCelsius(float x)
     return (1.8*x) + 32;
 }
 
+float grausFahr(float x)
+{
+    return (x-32) / 1.8;
+}
+
 int main()								
 {
     float g;
-    cout << "Quantos graus Celsius? --> ";
+    char op;
+    do{
+    
+    cout << "Quantos graus ? --> ";
     cin >> g;
-    cout << "\n\t"<< g << " Celsius = "<< setprecision(2)<<fixed<< grausCelsius(g) <<" Fahrenheit\n";
+    cout << "Celsius ou Fahrenheit[(s) para sair]? (c/f)--> ";
+    cin >> op;
+    op = toupper(op);
+    
+    switch (op)
+    {
+    case 'C':
+        cout << "\n\t"<< g << " Celsius = "<< setprecision(2)<<fixed<< grausCelsius(g) <<" Fahrenheit\n";
+        break;
+    case 'F':
+        cout << "\n\t"<< g << " Fahrenheit = "<< setprecision(2)<<fixed<< grausFahr(g) <<" Celsius\n";
+        break;  
+    case 'S':
+        exit(1);
+        break;
+    default:
+        cout << "Opção inválida\n";
+        break;
+    }
+    }while (true);
+    cout << "Encerrando ...\n";
+   
     system("pause");				
 	return 0;	
 }
