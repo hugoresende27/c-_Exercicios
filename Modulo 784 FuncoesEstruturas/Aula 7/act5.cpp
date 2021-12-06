85980081768394b50a279c99b61d05b53e0d6d08
hugoresende27 <Hugoresende27@gmail.com>
2021-12-06 17:52:41 +0000

    a

--- Modulo 784 FuncoesEstruturas/Aula 7/act5.cpp
+++ Modulo 784 FuncoesEstruturas/Aula 7/act5.cpp
@@ -14,10 +14,8 @@ using namespace std;
 
 bool primo(int num){
   int contador=0;
-  for (int i=1 ; i<=num ; i++)
-    {
-        if (num % i == 0)
-        {
+  for (int i=1 ; i<=num ; i++){
+        if (num % i == 0){
             contador++;
         }
     }
@@ -31,24 +29,43 @@ bool primo(int num){
     }
 }
 
+void imprime_vetor(int *vet, int t){
+    for (int i =0; i<t; i++){
+        if (*(vet+i) !=0 ){
+            //cout << vet[i]<< " - ";
+           // printf ("%d", *(vet+i));        //*vet = vetPrimos[0]
+            cout << *(vet+i) << " - ";
+        }    
+    }
+}
+
 int main()								
 {
     int inicio,fim;
-    int vetPrimos[500];
+    
     int cont=0;
     cout << "Qual o limite INICIAL?--> ";
     cin >> inicio;
     cout << "Qual o limite FINAL?--> ";
     cin >> fim;
-    for (int i=inicio; i<fim; i++){
+    int tam = (fim-inicio);
+    int vetPrimos[tam+1];
+    for (int i=inicio; i<=fim; i++){
         if (primo(i)){
             vetPrimos[cont]=i;
-            cont++;
+        } else {
+            vetPrimos[cont] = 0;
         }
+        cont++;
     }
+    /*
     for (int i=0; i<cont;i++){
         cout << vetPrimos[i]<<" | ";
+        //cout << &vetPrimos[i]<< " # ";
     }
+    cout <<"SIZE OF "<<sizeof(vetPrimos)/4<<endl;
+*/
+    imprime_vetor(vetPrimos,cont);
 
     system("pause");				
 	return 0;	
