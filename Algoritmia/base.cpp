16b8b9ce7856c593e9c99ceefab37b96ad3a5387
hugoresende27 <Hugoresende27@gmail.com>
2022-01-12 09:54:09 +0000

    l

--- Algoritmia/base.cpp
+++ Algoritmia/base.cpp
@@ -1,11 +1,16 @@
 #include <iostream>	
 #include <string.h>
 #include <math.h>		
+#include <locale.h>
 using namespace std;
 
 int main()								
 {
-  
+
+    //setlocale (LC_ALL, "Portuguese");
+
+    
+
     cout << endl;
     system("pause");				
 	return 0;	
