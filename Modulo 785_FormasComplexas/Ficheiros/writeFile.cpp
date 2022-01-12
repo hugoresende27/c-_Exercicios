c8fe586bd6cce2804f306fc1146d7babfd51d000
hugoresende27 <Hugoresende27@gmail.com>
2022-01-12 12:50:31 +0000

    a

--- Modulo 785_FormasComplexas/Ficheiros/writeFile.cpp
+++ Modulo 785_FormasComplexas/Ficheiros/writeFile.cpp
@@ -8,13 +8,44 @@ int main()
 {
 
     FILE *fich;
-    fich = fopen("texto1.txt", "wt");//com "w" não faz sentido verificar se ficheiro existe, porque se não existir é criado
+    
+    fich = fopen("texto1.txt", "w");//com "w" não faz sentido verificar se ficheiro existe, porque se não existir é criado
 
     cout << "A escrever no ficheiro\n";
     fprintf(fich, "%d %d %d", 12,10,2022 );
     fprintf(fich, "%s", "\nhugao");
     fprintf(fich,  "\nResende");
     fclose(fich);
+    
+    char texto[100];
+    char frase[100];
+
+    fich = fopen("texto1.txt" ,"r");
+
+     //verificação se o ficheiro existe
+    if (fich == NULL ){
+        printf ("Falhou abertura do ficheiro");
+    }
+    else {
+        cout << "Ficheiro aberto com sucesso\n";
+
+        //fscanf(fich, "%s", &texto ); //fscanf não lê do espaço para a frente
+        //cout << texto;
+        //fgets(texto,100, fich);
+        //puts(texto);
+
+        //fputs(texto,fich);
+        //puts(texto);
+
+        while (fgets(frase,100,fich) != NULL) 
+        {
+            printf ("O texto que o ficheiro tem :: %s", frase);
+        }
+      
+        fclose(fich);
+    }
+
+    
 
     cout << endl;
     system("pause");				
