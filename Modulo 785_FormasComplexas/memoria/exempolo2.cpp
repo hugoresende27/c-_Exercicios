d7751f5a6dc8c6de61a17a7ef1a2179277e5c23c
hugoresende27 <Hugoresende27@gmail.com>
2022-01-31 15:03:37 +0000

    a

--- Modulo 785_FormasComplexas/memoria/exempolo2.cpp
+++ Modulo 785_FormasComplexas/memoria/exempolo2.cpp
@@ -8,18 +8,32 @@
 //https://www.youtube.com/watch?v=yFboyOwk2oM&ab_channel=CodeVault
 using namespace std;
 
+void fail (){
+    cout << "Falhou!";
+    exit(EXIT_FAILURE);
+}
+
 int main()								
 {
 
-    //int *p =(int*) malloc(sizeof(int)); //alocar memória
+    int *z =(int*) malloc(sizeof(int)); //alocar memória
     int *p =(int*) calloc(3,sizeof(int)); //alocar memória
-    //*p = 5;
-    //printf ("o valor é %d", *p);
+    *z = 5;
+    printf ("o valor é %d", *z);
     p[1]=3;
     p[2]=90;
   
     printf ("o valor é %d e %d teste %d", p[1],p[2],p[0]); //p[0] vai ter um valor de memória
     free(p);    //desalocar/libertar memória
+    free(z);
+
+    char *name = (char*)malloc(4*sizeof(char));
+    if (name == NULL) fail();
+    strcpy(name,"hugo");
+    cout << name;
+
+    free(name);
+
     
     cout << endl;
     system("pause");				
